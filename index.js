1bdab42a957749790627978443a6bb1e7e5da816
snyder444k <snyderjack100@gmail.com>
2025-08-07 00:46:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+$('.gbogbo').addClass('active');
+
+$('.all').click( function () {
+      $('.all').removeClass('active');
+      $(this).addClass('active');
+});
+
+$('.gbogbo').click( function(){
+      $('.box').show();
+} )
+
+
+  var mode = $('#sun');
+
+  mode.on("click",function () {
+      $('body').toggleClass('light-theme');
+
+      if ($('body').hasClass('light-theme')) {
+            mode[0].src = "./icon-moon.svg";
+         
+      } else {
+          mode[0].src = "./icon-sun.svg";
+      }
+  });
+
+
+  $('#active').click( function() {
+     $('.box').hide();
+     $('.box').has('input.check:checked').show();
+      
+  });
+
+  $('#inactive').click( function() {
+      $('.box').hide();
+      $('.box').has('input.check:not(:checked)').show();
+  });
+
+  $('.remove').click( function() {
+      $('.box').has(this).hide();
+  })
